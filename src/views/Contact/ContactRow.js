60b4f84b23fc0dac5b0d7043b6c017456b5bcefe
Devan Grose <devangrose@gmail.com>
2018-12-04 13:05:04 -0800

    made contact cards links

--- src/views/Contact/ContactRow.js
+++ src/views/Contact/ContactRow.js
@@ -14,6 +14,12 @@ const styles = theme => ({
   },
   text: {
     marginBottom: '4rem'
+  },
+  a: {
+    color: 'inherit',
+    '&:hover': {
+      color: 'inherit'
+    }
   }
 });
 
@@ -28,13 +34,19 @@ function GetInTouchRow(props) {
       {heading}
       <Grid container spacing={24}>
         <Grid item xs={12} sm={12} md={4}>
+          <a className={classes.a} href="https://www.google.com/maps/place/Trillium+Ink/@47.971999,-122.2038394,17z/data=!3m1!4b1!4m5!3m4!1s0x549aaa98189b4f1b:0x3525e7727143a351!8m2!3d47.971999!4d-122.2016507:" target="_blank">
           <ContactCard hideLearn={true} icon="home" header="Address" content={<p>3418 Broadway<br/> Everett, Wa 98201</p>}/>
+        </a>
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
+          <a className={classes.a} href="tel:425-258-6245">
           <ContactCard hideLearn={true} icon="phone" header="Phone" content={<p>Office (425) 258-6245<br/> Mobile (425) 238-1222</p>}/>
+          </a>
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
-          <ContactCard hideLearn={true} icon="email" header="Email" content="inkbypam@gmail.com"/>
+          <a className={classes.a} href="mailto:inkbypam@gmail.com">
+            <ContactCard hideLearn={true} icon="email" header="Email" content="inkbypam@gmail.com"/>
+          </a>
         </Grid>
       </Grid>
     </div>
