3374ee8b99c1f29e914d48e8b7094aededc4d7b8
Devan Grose <devangrose@gmail.com>
2018-12-04 13:32:58 -0800

    added titles to cards

--- src/views/Transformations/Card.js
+++ src/views/Transformations/Card.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { CardMedia, Card } from '@material-ui/core';
+import { CardContent, Card, CardMedia } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import HomeServiceCard from 'views/Home/HomeServiceCard.js';
 
 const styles = {
    media: {
@@ -13,8 +14,7 @@ const styles = {
    },
    card: {
       position: 'relative',
-      width: "50vh",
-      maxWidth: "80vw",
+      width: "100%",
       height: "50vh", 
       display: "inline-block",
       marginTop: "25px"
@@ -29,8 +29,10 @@ const styles = {
      width: "100%",
    },
    image: {
-     width: "100%",
-     height: "100%",
+     width: "auto",
+     maxHeight: "80%",
+     margin: 'auto',
+     padding: '3%',
    }
 };
 
@@ -53,23 +55,32 @@ class CardLink extends Component {
     return (
       <Link to={this.props.link} style={{maxWidth: "80%", height: "100%", textDecoration: "none"}}>
         <Card style={styles.card} onMouseEnter={this.mouseOver} onMouseLeave={this.mouseLeave}>
-        <img src={this.props.image} style={styles.image}/>
-         <div style={{
-          position: 'absolute',
-          top: '0px',
-          left: '0px',
-          color: 'white',
-          textAlign: 'center', 
-          backgroundColor: 'rgba(0,0,0,.4)',
-          height: "100%",
-          width: "100%",
-          display: this.state.show,
-          padding: "50% 0",
-          }}>
-              <Typography variant="h4" style={{color: 'white', fontWeight: "bold"}}>
+          <CardMedia 
+              component="img"
+              src={this.props.image}
+              style={styles.image}
+            />
+           <div style={{
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            color: 'white',
+            textAlign: 'center', 
+            backgroundColor: 'rgba(0,0,0,.4)',
+            height: "100%",
+            width: "100%",
+            display: this.state.show,
+            padding: "50% 0",
+            }}>
+                <Typography variant="h4" style={{color: 'white', fontWeight: "bold"}}>
+                  {this.props.name}
+                </Typography>
+           </div>
+           <CardContent>
+              <Typography variant="body1" style={{fontWeight: "bold"}}>
                 {this.props.name}
               </Typography>
-         </div>
+           </CardContent>
         </Card>
       </Link>
 
--- src/views/Transformations/Transformations.js
+++ src/views/Transformations/Transformations.js
@@ -8,14 +8,14 @@ import HeroImage from '../../Components/HeroImage.js';
 import CardLink from './Card.js';
 import redColor from '../../color.js';
 
-import menHairLoss from '../../assets/mens-hair-loss1.jpg';
+import menHairLoss from '../../assets/mens-hair-loss3.jpg';
 import menAlopecia from '../../assets/mens-alopcia2.jpg';
 import menTransplant from '../../assets/men-transplant1.jpg';
 
 import womenHairLoss from '../../assets/women-hair-loss1.jpg';
 import womenAlopecia from '../../assets/women-alopecia1.jpg';
 import eyebrows from '../../assets/eyebrows1.jpg';
-import eyebrows3d from '../../assets/3d-eyebrows1.jpg';
+import eyebrows3d from '../../assets/3d-eyebrows2.jpeg';
 import eyeliner from '../../assets/eyeliner1.jpg';
 import lips from '../../assets/lips1.jpg';
 
@@ -25,7 +25,7 @@ const styles = {
   img: {
     width: '80vw'
   }
-}
+};
 
 class Transformations extends Component {
 
@@ -40,7 +40,7 @@ class Transformations extends Component {
           <Typography variant="h6" style={{color: redColor}}>
             Click for more info!
           </Typography>
-          <Grid container >
+          <Grid container spacing={24} >
             <Grid item md={4} sm={12}>
               <CardLink image={menHairLoss} name="Men's Hair Loss" link="/service/men/hair-loss-treatment"/>
             </Grid>
@@ -48,7 +48,7 @@ class Transformations extends Component {
               <CardLink image={menAlopecia} name="Men's Alopecia" link="/service/men/alopecia"/>
             </Grid>
             <Grid item md={4} sm={12}>
-              <CardLink image={menTransplant} name="Men's Hair Loss" link="/service/men/transplant-scars"/>
+              <CardLink image={menTransplant} name="Transplant Scars" link="/service/men/transplant-scars"/>
             </Grid>
           </Grid>
           <Typography variant="display2" style={{marginTop: "25px", color: redColor}}>
@@ -57,7 +57,7 @@ class Transformations extends Component {
           <Typography variant="h6" style={{color: redColor}}>
             Click for more info!
           </Typography>
-          <Grid container>
+          <Grid container spacing={24}>
             <Grid item md={4} sm={12}>
               <CardLink image={womenHairLoss} name="Women's Hair Loss" link="/service/women/hair-loss-treatment"/>
             </Grid>
