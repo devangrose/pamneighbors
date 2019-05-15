2c93ff4d2c65384bda0639828dff3a2b0969b4c8
Devan Grose <devangrose@gmail.com>
2019-05-14 22:36:42 -0700

    updated deploy script to save backups to s3

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-mv $(pwd)/build/index.html $(pwd)/build/home.html
-cp $(pwd)/build/home.html $(pwd)/build/404.shtml
-rm $(pwd)/build/index.html
+#mv $(pwd)/build/index.html $(pwd)/build/home.html
+#cp $(pwd)/build/home.html $(pwd)/build/404.shtml
+#rm $(pwd)/build/index.html
 
-scp -r $(pwd)/build/* wgu8ry0ccyvh@160.153.77.104:~/public_html
+#scp -r $(pwd)/build/* wgu8ry0ccyvh@160.153.77.104:~/public_html
+
+zip -r $(pwd)/build_$(date +'%Y-%m-%d').zip $(pwd)/build
+aws s3 cp $(pwd)/build_$(date +'%Y-%m-%d').zip s3://pamneighbors-backup/
+rm $(pwd)/build_$(date +'%Y-%m-%d').zip
